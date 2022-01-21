f1b7145bda79598534ae6448e15ba32df05ef728
youssefahmeddevlogs <youssefahmedsup@gmail.com>
2022-01-21 16:38:49 +0200

    filter-component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,40 @@ import Filter from "./components/Filter/Filter";
 
 function App() {
   const [products, setProducts] = useState(data);
+  const [category, setCategory] = useState("");
+  const [sort, setSort] = useState("");
+
+  const handelFilterByCategory = (e) => {
+    setCategory(e.target.value);
+
+    if (e.target.value == "all") {
+      setProducts(data);
+    } else {
+      let productsClone = [...data];
+      let newProducts = productsClone.filter(
+        (product) => product.category == e.target.value
+      );
+      setProducts(newProducts);
+    }
+  };
+
+  const handelSortByPrice = (e) => {
+    let order = e.target.value;
+    setSort(order);
+
+    let productsClone = [...products];
+    let newProducts = productsClone.sort((a, b) => {
+      if (order == "lowest") {
+        return a.price - b.price;
+      } else if ((order = "highest")) {
+        return b.price - a.price;
+      } else {
+        return a.id < b.id ? 1 : -1;
+      }
+    });
+
+    setProducts(newProducts);
+  };
 
   return (
     <div className="layout">
@@ -15,7 +49,13 @@ function App() {
       <main>
         <div className="container">
           <div className="wrapper">
-            <Filter products={products} />
+            <Filter
+              products={products}
+              handelFilterByCategory={handelFilterByCategory}
+              handelSortByPrice={handelSortByPrice}
+              category={category}
+              sort={sort}
+            />
             <Products products={products} />
           </div>
         </div>
